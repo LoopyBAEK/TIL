5707e1313cf85bd5abf364b14fdd72d32b559cc4
Loopy_BAEK <likipop@naver.com>
2023-03-16 16:31:06 +0900

    2023-03-15 BOJ

---
+++ src/boj/bronze/bronzeIV/기찍N_2742.java
@@ -0,0 +1,14 @@
+package boj.bronze.bronzeIV;
+
+import java.util.Scanner;
+
+public class 기찍N_2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = n; i > 0; i--){
+            System.out.println(i);
+        }
+    }
+    // 2023-03-15
+}
