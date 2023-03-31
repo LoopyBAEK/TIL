beb29eb497e454bf2bf273e0e12742455f78afea
Loopy_BAEK <likipop@naver.com>
2023-04-01 00:41:25 +0900

    2023-03-31 BOJ

---
+++ src/boj/bronze/bronzeV/Welovekriii_10718.java
@@ -0,0 +1,9 @@
+package boj.bronze.bronzeV;
+
+public class Welovekriii_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+    // 2023-04-01
+}
