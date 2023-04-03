65ddad1839822ae270d33a24d87c3ad8a6b0ee90
Loopy_BAEK <likipop@naver.com>
2023-04-03 16:25:43 +0900

    2023-04-02 2023 고려대학교x연세대학교 프로그래밍 경시대회 Open Contest

---
+++ src/boj/koreaXyonsei2023/A번_당신은운명을믿나요.java
@@ -0,0 +1,45 @@
+package boj.koreaXyonsei2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class A번_당신은운명을믿나요 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        ArrayList<Character> list = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+            list.add(s.charAt(i));
+        }
+
+        String korea = "KOREA";
+        int koreaCount = 0;
+        String yonsei = "YONSEI";
+        int yonseiCount = 0;
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i) == korea.charAt(koreaCount) && list.get(i) == yonsei.charAt(yonseiCount)){
+                koreaCount++;
+                yonseiCount++;
+            }else if(list.get(i) == korea.charAt(koreaCount)){
+                koreaCount++;
+                if(koreaCount == 5){
+                    break;
+                }
+            }else if(list.get(i) == yonsei.charAt(yonseiCount)){
+                yonseiCount++;
+                if(yonseiCount == 6){
+                    break;
+                }
+            }
+        }
+
+        if(koreaCount == 5){
+            System.out.println("KOREA");
+        }else if(yonseiCount == 6){
+            System.out.println("YONSEI");
+        }
+    }
+    // 2023-04-02
+}
---
+++ src/boj/koreaXyonsei2023/B번_ParityConstraintClosestPair.java
@@ -0,0 +1,65 @@
+package boj.koreaXyonsei2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B번_ParityConstraintClosestPair {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 점의 개수
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[n];
+        for(int i = 0; i < n; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        int odd = 0;
+        int even = 0;
+
+        int idx0 = numbers[0];
+        int idx1 = numbers[1];
+        if((idx1 - idx0) % 2 == 0){
+            even = idx1 - idx0;
+        }else{
+            odd = idx1 - idx0;
+        }
+
+        for(int i = 2; i < numbers.length; i++){
+            if((numbers[i] - numbers[i - 1]) % 2 == 1){
+                if(odd == 0 || numbers[i] - numbers[i - 1] < odd){
+                    odd = numbers[i] - numbers[i - 1];
+                }
+            }else if((numbers[i] - numbers[i - 2]) % 2 == 1){
+                if(odd == 0 || numbers[i] - numbers[i - 2] < odd){
+                    odd = numbers[i] - numbers[i - 2];
+                }
+            }
+
+            if((numbers[i] - numbers[i - 1]) % 2 == 0){
+                if(even == 0 || numbers[i] - numbers[i - 1] < even){
+                    even = numbers[i] - numbers[i - 1];
+                }
+            }else if((numbers[i] - numbers[i - 2]) % 2 == 0){
+                if(even == 0 || numbers[i] - numbers[i - 2] < even){
+                    even = numbers[i] - numbers[i - 2];
+                }
+            }
+        }
+
+
+        if(odd == 0){
+            odd = -1;
+        }else if(even == 0){
+            even = -1;
+        }
+        System.out.printf("%d %d", even, odd);
+
+    }
+    // 2023-04-02
+}
