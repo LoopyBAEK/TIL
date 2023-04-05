c311f4d8d0fbc4564754256ce652ce4c4d5cbae2
Loopy_BAEK <likipop@naver.com>
2023-04-05 15:24:17 +0900

    2023-04-05 BOJ

---
+++ src/boj/bronze/bronzeV/천구백구십팔년생인내가태국에서는2541년생_18108.java
@@ -0,0 +1,12 @@
+package boj.bronze.bronzeV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 천구백구십팔년생인내가태국에서는2541년생_18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) - 543);
+    }
+}
