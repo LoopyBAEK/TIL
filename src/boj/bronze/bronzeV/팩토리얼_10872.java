674aa93f81e3663717d277e23df980450192f1d7
Loopy_BAEK <likipop@naver.com>
2023-04-06 19:50:54 +0900

    2023-04-06 BOJ

---
+++ src/boj/bronze/bronzeV/팩토리얼_10872.java
@@ -0,0 +1,23 @@
+package boj.bronze.bronzeV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 팩토리얼_10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int result = 1;
+        if(n == 0){
+            System.out.println(1);
+        }else{
+            for(int i = n; i > 0; i--){
+                result *= i;
+            }
+            System.out.println(result);
+        }
+    }
+    // 2023-04-06
+}
