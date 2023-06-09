410af4f8c450d47480e2dc4718b6e0a56d1072c0
Loopy_BAEK <likipop@naver.com>
2023-06-09 15:05:41 +0900

    2023-06-09 BOJ(11931)

---
+++ src/boj/silver/silverV/수정렬하기4_11931.java
@@ -0,0 +1,30 @@
+package boj.silver.silverV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 수정렬하기4_11931 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Integer[] numbers = new Integer[n];
+        for(int i = 0; i < n; i++){
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(numbers, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < numbers.length; i++){
+            sb.append(numbers[i] + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    // 2023-06-09
+}
