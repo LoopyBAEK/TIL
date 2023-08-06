1ca77e816269b1a6c2758ead974b86da44e77209
Loopy_BAEK <likipop@naver.com>
2023-08-06 15:19:55 +0900

    2023-08-06 BOJ (A번_양말 짝 맞추기) solved.ac Grand Arena #1 · Arena #1

---
+++ src/boj/SolvedacGrandArena/Arena1/A번_양말짝맞추기.java
@@ -0,0 +1,29 @@
+package boj.SolvedacGrandArena.Arena1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class A번_양말짝맞추기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            int curr = Integer.parseInt(br.readLine());
+            if(!list.isEmpty()){
+                if(list.contains(curr)){
+                    int idx = list.indexOf(curr);
+                    list.remove(idx);
+                }else{
+                    list.add(curr);
+                }
+            }else{
+                list.add(curr);
+            }
+        }
+
+        System.out.println(list.get(0));
+    }
+}
